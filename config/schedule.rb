0757fb42cc581ac6e4a95bb1bb8a3a6e4704fe88
Ash McKenzie <ash@greenworm.com.au>
2012-05-29 23:49:08 +1000

    argh

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,6 +7,6 @@ require File.expand_path('../../lib/init', __FILE__)
 set :base, "#{ENV['HOME']}/#{$CONFIG.name}/current"
 set :output, "#{base}/log/cron.log"
 
-send(:every, eval($CONFIG.app.cron.frequency), eval("{ #{$CONFIG.app.cron.options} }")) do
+send(:every, eval($CONFIG.cron.frequency), eval("{ #{$CONFIG.cron.options} }")) do
   command "cd #{base} && ERRBIT_ENABLE=true APP_ENV=production ./scripts/new_dvds --verbose"
 end
